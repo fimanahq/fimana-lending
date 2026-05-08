812e2a465779f99414d54d75047e1c85a74625f2
joelverg0920 <joelaquinovergara@gmail.com>
2026-05-08 09:07:49 +0800

    Updated loan detail so Overall Profit and Overall Profit %

--- components/loan-list/loan-detail.tsx
+++ components/loan-list/loan-detail.tsx
@@ -86,7 +86,7 @@ export function LoanDetail({ loanId }: LoanDetailProps) {
 
   const currency = loan.loanProduct.currency || 'PHP'
   const schedule = loan.schedule ?? []
-  const overallProfitAmountMinor = schedule.reduce((sum, row) => sum + row.scheduledInterestAmountMinor, 0)
+  const overallProfitAmountMinor = loan.balances.interestPaidAmountMinor + loan.balances.interestOutstandingAmountMinor
   const overallProfitPercentage = loan.principalAmountMinor > 0
     ? (overallProfitAmountMinor / loan.principalAmountMinor) * 100
     : 0
